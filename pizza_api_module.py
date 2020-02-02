89146610a5a756a1cdcda4114a50771e6f35dd03
Tyler Li <teli@uci.edu>
2020-02-02 03:05:14 -0800

    order confirmation

--- pizza_api_module.py
+++ pizza_api_module.py
@@ -1,19 +1,21 @@
 from pizzapy import *
 
-#Wisconsin 6 Cheese Pizza
-#Hand Tossed Pizza
+#Wisconsin 6 Cheese Pizza "thin wisconsin"
+#Brooklyn Pizza "brooklyn"
 
-def order_pizza(first, last, email, phone_number, address):
+def order_pizza(first, last, email, phone_number, address, pizza):
     try:
         #customer = Customer('Barack', 'Obama', 'barack@whitehouse.gov', '2024561111',
          #                   '700 Pennsylvania Avenue NW, Washington, DC, 20408')
         customer = Customer(first, last, email, phone_number, address)
         my_local_dominos = StoreLocator.find_closest_store_to_customer(customer)
         menu = my_local_dominos.get_menu()
-        print(menu.search(Name = 'Pizza'))
         card = CreditCard('4100123422343234', '0115', '777', '90210')
         order = Order.begin_customer_order(customer, my_local_dominos)
-        order.add_item("20BCOKE")
+        if pizza == "thin wisconsin":
+            order.add_item("P14IRECZ")
+        elif pizza == "brooklyn":
+            order.add_item("PBKIREZA")
         order.place(card)
         return my_local_dominos.place_order(order, card)
     except:
--- pizza_main.py
+++ pizza_main.py
@@ -3,15 +3,16 @@ from twilio.twiml.messaging_response import MessagingResponse
 import os
 from twilio.rest import Client
 import api_keys
+import pizza_api_module
 
 app = Flask(__name__)
 
 votes = 0
+total_responses = 0
 dict_of_info = {"+16197219618": ["Emily", ""], "+18089310909": ["Tyler", ""], "+17145144501": ["Bryant", ""]}
 list_of_nums = ["+18089310909"]  # "+16197219618", "+17145144501"] UNCOMMENT LATER
 stage = 1
-max = 3
-sent = False
+max = 1
 client = Client(api_keys.TWILIO_ACCOUNT_SID, api_keys.TWILIO_AUTH_TOKEN)
 
 def send_message(stage, num):
@@ -23,7 +24,7 @@ def send_message(stage, num):
             )
     if stage == 2:
         client.messages.create(
-                body='Which pizza?',
+                body='Which pizza? Choose between Brooklyn or Thin Wisconsin.',
                 from_='+14344736168',
                 to=num
             )
@@ -33,10 +34,22 @@ def send_message(stage, num):
                 from_='+14344736168',
                 to=num
             )
+    if stage == 4:
+        client.messages.create(
+                body='Your order has been placed for Brooklyn!',
+                from_='+14344736168',
+                to=num
+            )
+    if stage == 5:
+        client.messages.create(
+                body='Your order has been placed for Thin Wisconsin!',
+                from_='+14344736168',
+                to=num
+            )
 
 
-def check_if_max(votes, max):
-    return votes == max
+def check_if_max(total_responses, max):
+    return total_responses == max
 
 def check_majority_vote(votes, max):
     if votes/max >= 0.5:
@@ -51,49 +64,62 @@ def sms_reply():
     global sent
     global dict_of_info
     global stage
+    global total_responses
 
     msg = request.values.get("Body", None)
     phone_number = request.values.get("From", None)
     resp = MessagingResponse()
 
-    if msg.lower() == "yes" and dict_of_info[phone_number][1] == "":
+    if msg.lower() == "yes" and dict_of_info[phone_number][1] == "" and stage == 1:
         resp.message(dict_of_info[phone_number][0] + " has voted yes for pizza!")
         dict_of_info[phone_number][1] = 1 #1 for yes
         votes += 1
-    elif msg.lower() == "no" and dict_of_info[phone_number][1] == "":
+        total_responses += 1
+    elif msg.lower() == "no" and dict_of_info[phone_number][1] == "" and stage == 1:
         resp.message(dict_of_info[phone_number][0] + " has voted no for pizza.")
         dict_of_info[phone_number][1] = 0  #0 for no
+        total_responses += 1
+    elif msg.lower() == "brooklyn" and dict_of_info[phone_number][1] == "" and stage == 2:
+        resp.message(dict_of_info[phone_number][0] + " has chosen Brooklyn pizza")
+        dict_of_info[phone_number][1] = "brooklyn"
         votes += 1
+        total_responses += 1
+    elif msg.lower() == "thin wisconsin" and dict_of_info[phone_number][1] == "" and stage == 2:
+        resp.message(dict_of_info[phone_number][0] + " has chosen Thin Wisconsin pizza")
+        dict_of_info[phone_number][1] = "thin wisconsin"
+        total_responses += 1
     else:
         resp.message("You have either entered an invalid response or already voted.")
 
-    if check_if_max(votes, max):
+    if check_if_max(total_responses, max):
         if stage == 2:
             if check_majority_vote(votes, max):
-                #use pizza_api_module
-            stage = 5
+                for num in list_of_nums:
+                    pizza_api_module.order_pizza(dict_of_info[num][0], "Appleseed", "someone@uci.edu", num, '2272 Michelson Dr, Irvine, CA, 92612', "brooklyn")
+                    send_message(4, num)
+            else:
+                for num in list_of_nums:
+                    pizza_api_module.order_pizza(dict_of_info[num][0], "Appleseed", "someone@uci.edu", num, '2272 Michelson Dr, Irvine, CA, 92612', "thin wisconsin")
+                    send_message(5, num)
+            stage = 6
         elif stage == 1:
             if check_majority_vote(votes, max):
                 stage += 1
                 votes = 0
-                sent = False
+                total_responses = 0
                 dict_of_info = {"+16197219618": ["Emily", ""], "+18089310909": ["Tyler", ""],
                                 "+17145144501": ["Bryant", ""]}
                 for num in list_of_nums:
                     send_message(2, num)
-                sent = True
             else:
                 for num in list_of_nums:
                     send_message(3, num)  # stage 3 = No pizza
-                    stage = 5
-                    sent = True
+                stage = 6
 
     return str(resp)
 
 
 if __name__ == "__main__":
-    if sent == False:
-        for num in list_of_nums:
-            send_message(1, num)
-        sent = True
+    for num in list_of_nums:
+        send_message(1, num)
     app.run(port=5000, debug=True)
